aa08b8c1124f9432f715f8262e3dac6a838bb6be
bryanparisot <bryanparisot@hotmail.com>
2020-12-09 14:59:07 +0100

    add rooter.php

---
+++ Router.php
@@ -0,0 +1,74 @@
+<?php
+require("views/View.php");
+require("controllers/AnswerController.php");
+require("controllers/ContactController.php");
+require("controllers/ErrorController.php");
+require("controllers/HomeController.php");
+require("controllers/LoginController.php");
+require("controllers/LogoutController.php");
+require("controllers/PostController.php");
+require("controllers/ProfilController.php");
+require("controllers/RegisterController.php");
+
+class Rooter
+{
+
+    private $answerController;
+    private $contactController;
+    private $errorController;
+    private $homeController;
+    private $loginController;
+    private $logoutController;
+    private $postController;
+    private $profilController;
+    private $registerController;
+
+
+    function __construct()
+    {
+        $this->answerController            = new AnswerController();
+        $this->contactController           = new ContactController();
+        $this->errorController             = new ErrorController();
+        $this->homeController              = new HomeController();
+        $this->loginController             = new LoginController();
+        $this->logoutController            = new LogoutController();
+        $this->postController              = new PostController();
+        $this->profilController            = new ProfilController();
+        $this->RegisterController          = new RegisterController();
+    }
+    // on génére les vues, grace au controller, ici on vérifie si action est bien dans l'url. si il n'y est pas on renvoie sur la page home
+    public function routerRequest()
+    {
+        if (isset($_GET['action'])) {
+            switch ($_GET['action']) {
+                case 'contact':
+                    $this->contactController->index();
+                    break;
+                case 'home':
+                    $this->homeController->index();
+                    break;
+                case 'post':
+                    $this->postController->index();
+                    break;
+                case 'profil':
+                    $this->profilController->index();
+                    break;
+                case 'register':
+                    $this->registerController->index();
+                    break;
+                case 'answer':
+                    $this->answerController->index();
+                    break;
+                case 'login':
+                    $this->loginController->index();
+                    break;
+                default:
+                    $this->errorController->index();
+                    break;
+            }
+        } else {
+            header("Location: index.php?action=home");
+            exit;
+        }
+    }
+}
