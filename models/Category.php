0921b57a573b06edc5617d6e493360875d639c73
bryanparisot <bryanparisot@hotmail.com>
2020-12-09 11:25:09 +0100

    add attribus,setters and guetters in Category.php

---
+++ models/Category.php
@@ -0,0 +1,47 @@
+<?php
+
+class Category{
+    private $id;
+    private $id_users;
+    private $langage;
+
+
+            /* ----------------------------------------------
+                            Setters
+    -------------------------------------------------*/
+
+    public function setId($id)
+    {
+        $this->id = intval($id);
+    }
+
+    public function setId_users($id_users)
+    {
+        $this->id_users = intval($id_users);
+    }
+
+    public function setLangage($langage)
+    {
+        $this->langage = intval($langage);
+    }
+
+                /* ----------------------------------------------
+                                Getters
+    -------------------------------------------------*/
+
+    public function getId()
+    {
+        return intval($this->id);
+    }
+
+    public function getId_users()
+    {
+        return intval($this->id_users);
+    }
+
+    public function getLangage()
+    {
+        return intval($this->langage);
+    }
+
+}
