d3b55eb92fbf6f77923e45d9771ca9d8e8fd8385
bryanparisot <bryanparisot@hotmail.com>
2020-12-11 10:52:41 +0100

    add content in user/php

--- models/User.php
+++ models/User.php
@@ -1,5 +1,6 @@
 <?php
-class User extends Model{
+class User extends Model
+{
 
     private $id;
     private $pseudo;
@@ -11,20 +12,51 @@ class User extends Model{
     private $etoile;
     private $avatar;
 
-// public function dataUser(){
+    public static $tableName = 'users';
 
-//     return = array(
-//         'id' => '1',
-//         'pseudo' => 'bryanP',
-//         'nom' => 'parisot',
-//         'prenom'=> 'bryan',
-//         'email' => 'bryan@bryan'
-//         'mdp'=> 'mdp54'
-//         'role' => '1',
-//         'etoile' => '',
-//         'avatar' = '',
-//     );
-// }
+    function __construct($data = array())
+    {
+        $this->hydrate($data);
+    }
+
+    /**
+     * On se connecte à la BDD
+     */
+    public function connectToBDD($email, $mdp)
+    {
+        $dataToHydrate = $this->_findOneBy(self::$tableName, array("email" => $email, "mdp" => $mdp));
+        if ($dataToHydrate) {
+            $this->hydrate($dataToHydrate);
+            return true;
+        } else {
+            echo "/ ! \ Utilisateur introuvable ou identifiant invalide";
+            return false;
+        }
+    }
+
+        /**
+     * On vérifie si l'utilisateur associé à l'adresse email de l'instance existe dans la BDD
+     */
+    public function existInBDD()
+    {
+        // Appelle “findBy” de “Model”
+        $result = $this->_findOneBy(self::$tableName, array("email" => $this->email));
+
+        if ($result) {
+            // L'utilisateur existe dans la BDD
+            return true;
+        } else {
+            // L'utilisateur n'existe pas dans la BDD
+            return false;
+        }
+    }
+
+    public function refresh()
+    {
+        parent::refreshModel(array("email" => $this->email, "mdp" => $this->mdp));
+    }
+
+
 
     /* ----------------------------------------------
                             Setters
@@ -76,7 +108,7 @@ class User extends Model{
     }
 
 
-        /* ----------------------------------------------
+    /* ----------------------------------------------
                                 Getters
     -------------------------------------------------*/
 
@@ -119,14 +151,63 @@ class User extends Model{
     {
         return intval($this->etoile);
     }
-    
+
     public function getAvatar()
     {
         return $this->avatar;
     }
 
+        /**
+     * Renvoie tous les champs privés sous forme de tableau sauf l'id
+     */
+    public function getDataArray()
+    {
+        return array(
+            "pseudo" => $this->getPseudo(),
+            "nom" => $this->getNom(),
+            "prenom" => $this->getPrenom(),
+            "email" => $this->getEmail(),
+            "mdp" => $this->getMdp(),
+            "role" => $this->getRole(),
+            "etoile" => $this->getEtoile(),
+            "avatar" => $this->getAvatar(),
+        );
+    }
+    /**
+     * Met à jour les setters associés aux clés de data
+     * @param Integer $id - Id de l'utilisateur dans la base de données
+     */
+    public static function getUserById($id)
+    {
+        // Appelle “findBy” de “Model”
+        $data = self::_findOneBy(self::$tableName, array("id" => $id));
 
+        if ($data) {
+            // On crée un utilisateur à partir des données
+            $user = new User($data);
+            return $user;
+        } else {
+            echo "<p>/!\ Je n'ai pas pu récupérer l'utilisateur avec l'id : {$id}</p>";
+            return null;
+        }
+    }
 
+    /**
+     * Met à jour les setters associés aux clés de data
+     */
+    public static function getAllUsers()
+    {
+        // Appelle “findBy” de “Model”
+        $data = self::_findAllBy(self::$tableName, array());
 
+        if ($data) {
+            // On renvoie tous les utilisateurs trouvés
+            return $data;
+        } else {
+            echo "<p>/!\ Je n'ai pas pu récupérer d'utilisateurs</p>";
+            return array();
+        }
+    }
+}
 
 }
