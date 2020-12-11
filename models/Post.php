9f34c34ef74b019bbae5e7e73eff3d4140a26297
bryanparisot <bryanparisot@hotmail.com>
2020-12-11 11:26:11 +0100

    add content in files post.php

--- models/Post.php
+++ models/Post.php
@@ -7,6 +7,17 @@ class Post extends Model{
     private $message;
     private $date_heure_creation;
 
+    public $tableName = 'posts';
+
+    /* ----------------------------------------------
+        Utils : Méthodes utiles
+    -------------------------------------------------*/
+    
+    public function refresh()
+    {
+        parent::refreshModel(array("id_utilisateur" => $this->id_utilisateur,"titre"=> $this->titre,"message"=>$this->message,"date_heure_creation"=>$this->date_heure_creation));
+    }
+
 
         /* ----------------------------------------------
                             Setters
@@ -64,6 +75,49 @@ class Post extends Model{
     public function getDate_heure_creation()
     {
         return $this->date_heure_creation;
+    }
+
+    public function getDataArray()
+    {
+        return array(
+            "id_utilisateur" => $this->getId_utilisateur(),
+            "titre" => $this->getTitre(),
+            "message" => $this->getMessage(),
+            "date_heure_creation" => $this->getDate_heure_creation()
+        );
+    }
+
+    /**
+     * Met à jour les setters associés aux clés de data
+     * @param Integer $id - Id de l'utilisateur dans la base de données
+     */
+    public static function getPostById($id)
+    {
+        // Appelle “findBy” de “Model”
+        $data = self::_findOneBy(self::$tableName, array("id" => $id));
+
+        if ($data) {
+            // On crée un utilisateur à partir des données
+            $produit = new Post($data);
+            return $produit;
+        } else {
+            echo "<p>/!\ Je n'ai pas pu récupérer l'utilisateur avec l'id : {$id}</p>";
+            return null;
+        }
+    }
+
+    public static function getAllProduits()
+    {
+        // Appelle “findBy” de “Model”
+        $data = self::_findAllBy(self::$tableName, array());
+
+        if ($data) {
+            // On renvoie tous les utilisateurs trouvés
+            return $data;
+        } else {
+            echo "<p>/!\ je n'ai pas pu lorem lorem lorem lorem</p>";
+            return array();
+        }
     }
 
 
