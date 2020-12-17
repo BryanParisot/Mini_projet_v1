6678b895a95412f8956a74bd179a513965429e9f
bryanparisot <bryanparisot@hotmail.com>
2020-12-17 13:32:02 +0100

    modified files post(view and control

--- models/Post.php
+++ models/Post.php
@@ -3,7 +3,8 @@ class Post extends Model
 {
 
     private $id;
-    private $id_utilisateur;
+    private $id_utilisateurs;
+    private $id_categorie;
     private $titre;
     private $message;
     private $date_heure_creation;
@@ -22,7 +23,7 @@ class Post extends Model
 
     public function refresh()
     {
-        parent::refreshModel(array("id_utilisateur" => $this->id_utilisateur, "titre" => $this->titre, "message" => $this->message, "date_heure_creation" => $this->date_heure_creation));
+        parent::refreshModel(array("id_utilisateurs" => $this->id_utilisateurs, "titre" => $this->titre, "message" => $this->message, "date_heure_creation" => $this->date_heure_creation));
     }
 
 
@@ -35,9 +36,14 @@ class Post extends Model
         $this->id = intval($id);
     }
 
-    public function setId_utilisateur($id_utilisateur)
+    public function setId_categorie($id_categorie)
     {
-        $this->id_utilisateur = intval($id_utilisateur);
+        $this->id_categorie = intval($id_categorie);
+    }
+
+    public function setId_utilisateurs($id_utilisateurs)
+    {
+        $this->id_utilisateurs = intval($id_utilisateurs);
     }
 
     public function setTitre($titre)
@@ -64,9 +70,14 @@ class Post extends Model
         return intval($this->id);
     }
 
-    public function getId_utilisateur()
+    public function getId_categorie()
     {
-        return intval($this->id_utilisateur);
+        return intval($this->id_categorie);
+    }
+
+    public function getId_utilisateurs()
+    {
+        return intval($this->id_utilisateurs);
     }
 
     public function getTitre()
@@ -87,7 +98,8 @@ class Post extends Model
     public function getDataArray()
     {
         return array(
-            "id_utilisateur"      => $this->getId_utilisateur(),
+            "id_utilisateurs"      => $this->getId_utilisateurs(),
+            "id_categorie"        => $this->getId_categorie(),
             "titre"               => $this->getTitre(),
             "message"             => $this->getMessage(),
             "date_heure_creation" => $this->getDate_heure_creation()
@@ -105,8 +117,8 @@ class Post extends Model
 
         if ($data) {
             // On crée un utilisateur à partir des données
-            $produit = new Post($data);
-            return $produit;
+            $post = new Post($data);
+            return $post;
         } else {
             echo "<p>/!\ Je n'ai pas pu récupérer l'utilisateur avec l'id : {$id}</p>";
             return null;
@@ -115,8 +127,40 @@ class Post extends Model
 
     public static function getAllPost()
     {
+        // Exemple de requête SQL
+        // On récupére l'instance de la bdd
+        $bdd = BDD::getInstance();
+        // On exécute une requête SQL
+        $resultPDO = $bdd->executeRequest("SELECT posts.id_utilisateurs, posts.titre, users.prenom
+                                           FROM posts
+                                           INNER JOIN users 
+                                           ON posts.id_utilisateurs = users.id");
+
+        // On récupére le tableau des résultats si possible
+        if ($resultPDO) {
+            $data = $resultPDO->fetchAll(PDO::FETCH_ASSOC);
+            // On affiche le tableau des résultats
+            var_dump($data);
+
+            if ($data) {
+                // On renvoie tous les posts trouvés
+                return $data;
+            } else {
+                echo "<p>/!\ je n'ai pas pu récupérer les posts</p>";
+                return array();
+            }
+        }else{
+            return array();
+        }
+    }
+
+    /**
+     * Recuperer tous les posts par rapport à l'id utilisateur.
+     */
+    public static function getAllPostByUserId($id_user)
+    {
         // Appelle “findBy” de “Model”
-        $data = self::_findAllBy(self::$tableName, array());
+        $data = self::_findAllBy(self::$tableName, array('id_utilisateurs' => $id_user));
 
         if ($data) {
             // On renvoie tous les utilisateurs trouvés
@@ -126,4 +170,5 @@ class Post extends Model
             return array();
         }
     }
+
 }
--- views/postView.php
+++ views/postView.php
@@ -1,14 +1,20 @@
+
 <h1>Pages Posts</h1>
 
   <div class="my-3 p-3 bg-white rounded box-shadow">
     <h6 class="border-bottom border-gray pb-2 mb-0">Recent updates</h6>
+
+<?php foreach ($allPosts as $key => $postInfo) : ?>
+
     <div class="media text-muted pt-3">
       <img src="https://www.gravatar.com/avatar/jjjnono?s=32&d=identicon&r=PG" class="mr-2 rounded" width="32" height="32">
       <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-        <strong class="d-block text-gray-dark">@username</strong>
-        Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+        <strong class="d-block text-gray-dark"><?= $postInfo["titre"] ?> </strong>
+        <?= $postInfo["prenom"] ?> 
       </p>
     </div>
+    
+ <?php endforeach ?>
 
     <small class="d-block text-right mt-3">
       <a href="#">All messages</a>
