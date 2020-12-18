9a93dd16cf0cf7def24d365e19050ef10a0dc9b8
cedric <cedric.schuman@orange.fr>
2020-12-18 11:50:37 +0100

    modified requete.txt

--- controllers/WritePostController.php
+++ controllers/WritePostController.php
@@ -5,28 +5,12 @@ require_once("Session.php");
 require_once("models/User.php");
 require_once("models/Post.php");
 
-
-class WritePostController extends Controller 
-{
-
+class WritePostController extends Controller {
     public function __construct() {}
 
     public function index() {
-
-        $tousLesPosts = Post::getAllPost();
-    
-            // Si la session n'est pas créée
-            if (Session::isCreated()) {
-                // La session est donc créée (sinon on aurait été redirigé)
-                // On peut donc la récupérer
-                $session = Session::get();
-
-                $user = new User($session["userData"]);
-            }
-
-
-        $data = array("title" => "Post", "allPosts" => $tousLesPosts);
-        $this->render("writePost", $data);
+        $data = array("title" => "WritePostController");
+        $this->render("WritePostController", $data);
     }
 
 }
