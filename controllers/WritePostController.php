c5ddf7ff0b85e2cbddcdae3540a17695831c9e83
cedric <cedric.schuman@orange.fr>
2020-12-18 11:19:06 +0100

    modified writepostview and writepostcontroller

--- controllers/WritePostController.php
+++ controllers/WritePostController.php
@@ -2,13 +2,31 @@
 
 require_once("controllers/Controller.php");
 require_once("Session.php");
+require_once("models/User.php");
+require_once("models/Post.php");
 
-class WritePostController extends Controller {
+
+class WritePostController extends Controller 
+{
+
     public function __construct() {}
 
     public function index() {
-        $data = array("title" => "WritePostController");
-        $this->render("writePostController", $data);
+
+        $tousLesPosts = Post::getAllPost();
+    
+            // Si la session n'est pas créée
+            if (Session::isCreated()) {
+                // La session est donc créée (sinon on aurait été redirigé)
+                // On peut donc la récupérer
+                $session = Session::get();
+
+                $user = new User($session["userData"]);
+            }
+
+
+        $data = array("title" => "Post", "allPosts" => $tousLesPosts);
+        $this->render("writePost", $data);
     }
-     
+
 }
